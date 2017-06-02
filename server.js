cc3b83a66eb777404bb24ab914ccd87e50601eee
Iván Caro <bonic17@gmail.com>
2017-06-02 15:48:20 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var http = require('http');
+/*var http = require('http');
 
 var server = http.createServer(function(request, response) {
 
@@ -11,3 +11,20 @@ var port = process.env.PORT || 1337;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
+*/
+
+
+var restify = require('restify');
+
+function respond(req, res, next) {
+  res.send('hello hhh' + req.params.name);
+  next();
+}
+
+var server = restify.createServer();
+server.get('/hello/:name', respond);
+server.head('/hello/:name', respond);
+
+server.listen(8080, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
