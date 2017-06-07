145050fe5c9ec9d636a2831990cf4e3c4d690f11
Iván Caro <ivanc@lagash.com>
2017-06-07 19:54:17 -0400

    messages

---
+++ deploy/botCode/server.js
@@ -0,0 +1,31 @@
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World 3!");
+
+});
+
+var port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
+/*
+
+
+var restify = require('restify');
+
+function respond(req, res, next) {
+  res.send('hello hhh' + req.params.name);
+  next();
+}
+
+var server = restify.createServer();
+server.get('/hello/:name', respond);
+server.head('/hello/:name', respond);
+
+server.listen(8080, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
+*/
